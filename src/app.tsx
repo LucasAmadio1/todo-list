70fcc875f20afb27742b6f4fe2e6ec00ea2b0e6d
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-06-20 22:08:32 -0300

    componente text

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
-export function App() {
-  return (
-    <h1 className="text-pink-dark text-2xl">Hello World!</h1>
-  )
-}
+import { Text } from "./components/text";
 
+export function App() {
+  return <Text className="text-green-base">teadasd</Text>;
+}
