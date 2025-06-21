b1562a25da17f5cf56aa66af6835dff24c5a2247
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-06-21 17:27:04 -0300

    components

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,50 @@
-import { Text } from "./components/text";
+import CheckIcon from './assets/icons/check.svg?react';
+import PencilIcon from './assets/icons/pencil.svg?react';
+import PlusIcon from './assets/icons/plus.svg?react';
+import SpinnerIcon from './assets/icons/spinner.svg?react';
+import TrashIcon from './assets/icons/trash.svg?react';
+import XIcon from './assets/icons/x.svg?react';
+import { Badge } from './components/badge';
+import { Button } from './components/button';
+import { ButtonIcon } from './components/button-icon';
+import { Icon } from './components/icon';
+import { Text } from './components/text';
 
 export function App() {
-  return <Text className="text-green-base">teadasd</Text>;
+  return (
+    <div className="flex flex-col gap-4 p-10">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá mundo!
+        </Text>
+        <Text className="text-green-base">Olá mundo!</Text>
+        <Text variant="body-md-bold">Olá mundo!</Text>
+        <Text>Levar o dog pra passear</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
+      </div>
+
+      <div>
+        <Button icon={PlusIcon}>Nova tarefa</Button>
+      </div>
+
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon} />
+        <ButtonIcon icon={TrashIcon} variant="secondary" />
+        <ButtonIcon icon={TrashIcon} variant="tertiary" />
+      </div>
+    </div>
+  );
 }
