3f1c46b57bc724c2a90e6b70532637c30a870064
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-06-22 14:58:07 -0300

    card e container

--- src/app.tsx
+++ src/app.tsx
@@ -7,54 +7,62 @@ import XIcon from './assets/icons/x.svg?react';
 import { Badge } from './components/badge';
 import { Button } from './components/button';
 import { ButtonIcon } from './components/button-icon';
+import { Card } from './components/card';
 import { Checkbox } from './components/checkbox';
+import { Container } from './components/container';
 import { Icon } from './components/icon';
 import { Input } from './components/input';
 import { Text } from './components/text';
 
 export function App() {
   return (
-    <div className="flex flex-col gap-10 p-10">
-      <div className="flex flex-col gap-2">
-        <Text variant="body-sm-bold" className="text-pink-base">
-          Olá mundo!
-        </Text>
-        <Text className="text-green-base">Olá mundo!</Text>
-        <Text variant="body-md-bold">Olá mundo!</Text>
-        <Text>Levar o dog pra passear</Text>
-      </div>
+    <Container>
+      <div className="flex flex-col gap-10 p-10">
+        <div className="flex flex-col gap-2">
+          <Text variant="body-sm-bold" className="text-pink-base">
+            Olá mundo!
+          </Text>
+          <Text className="text-green-base">Olá mundo!</Text>
+          <Text variant="body-md-bold">Olá mundo!</Text>
+          <Text>Levar o dog pra passear</Text>
+        </div>
 
-      <div className="flex gap-1">
-        <Icon svg={TrashIcon} className="fill-green-base" />
-        <Icon svg={CheckIcon} />
-        <Icon svg={PlusIcon} />
-        <Icon svg={SpinnerIcon} />
-        <Icon svg={PencilIcon} />
-        <Icon svg={XIcon} />
-      </div>
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} className="fill-green-base" />
+          <Icon svg={CheckIcon} />
+          <Icon svg={PlusIcon} />
+          <Icon svg={SpinnerIcon} />
+          <Icon svg={PencilIcon} />
+          <Icon svg={XIcon} />
+        </div>
 
-      <div>
-        <Badge variant="secondary">5</Badge>
-        <Badge variant="primary">2 de 5</Badge>
-      </div>
+        <div>
+          <Badge variant="secondary">5</Badge>
+          <Badge variant="primary">2 de 5</Badge>
+        </div>
 
-      <div>
-        <Button icon={PlusIcon}>Nova tarefa</Button>
-      </div>
+        <div>
+          <Button icon={PlusIcon}>Nova tarefa</Button>
+        </div>
 
-      <div className="flex gap-1">
-        <ButtonIcon icon={TrashIcon} />
-        <ButtonIcon icon={TrashIcon} variant="secondary" />
-        <ButtonIcon icon={TrashIcon} variant="tertiary" />
-      </div>
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant="secondary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" />
+        </div>
 
-      <div>
-        <Input />
-      </div>
+        <div>
+          <Input />
+        </div>
 
-      <div>
-        <Checkbox />
+        <div>
+          <Checkbox />
+        </div>
+
+        <div>
+          <Card size="md">Hello world!</Card>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './global.css';
 import { App } from './app';
+import { Container } from './components/container';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
